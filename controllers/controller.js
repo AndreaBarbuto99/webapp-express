2ab1450b947c901c5a672778de479b92207a9c42
Andrea Barbuto <barbutoandrea99@gmail.com>
2026-02-23 10:14:06 +0100

    feat: added reviews vote on the show

--- controllers/controller.js
+++ controllers/controller.js
@@ -15,15 +15,23 @@ function show(req, res) {
 
     const id = parseInt(req.params.id)
 
+    const showSqlReviews = "SELECT reviews.vote FROM reviews WHERE movie_id = ?"
+
     const showSql = "SELECT * FROM movies WHERE id = ?"
 
+
     connection.query(showSql, [id], (err, results) => {
+
         if (err) return res.json({ error: err.message });
-        res.json(results)
+        if (results.length === 0) return res.json({ error: "Movie non trovato" });
+
+        const movie = results[0];
+
+        connection.query(showSqlReviews, [id], (err, resultsReviews) => {
+            if (err) return res.json({ error: err.message });
+            movie.reviews = resultsReviews;
+            res.json(movie);
+        });
     });
-
 }
-
-
-
-module.exports = { show, index }
+module.exports = { show, index };
