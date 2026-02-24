2ebe42a00d55ec73f830c6c18dde541715f40998
Andrea Barbuto <barbutoandrea99@gmail.com>
2026-02-24 19:32:55 +0100

    feat: added middleware cors to let front connect

--- app.js
+++ app.js
@@ -1,13 +1,17 @@
 const express = require("express");
 const app = express();
+const cors = require("cors")
 const port = process.env.PORT;
-
+const myOrigin = process.env.ORIGIN;
 
 const router = require("./routers/router")
 
+app.use(cors({
+    origin: myOrigin
+}))
 
 app.get("/", (req, res) => {
-    app.send("<h1>ciao a tutti</h1>")
+    app.send("<h1>Homepage del server express</h1>")
 });
 
 app.use("/movies", router)
